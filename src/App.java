d25e47d89e153de2222188ea0f6c78078a7bfe5f
tlsdncjf <shinwc147369@gmail.com>
2023-12-20 16:20:34 +0900

    명언 class 생성,등록 된 명언 객체를 List에 저장

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -9,8 +11,9 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-		// trim() : 혹시 있을지 모르는 좌우 공백 제거
-		int lastId = 1;
+
+		int lastId = 0;
+		List<WiseSaying> wiseSayings = new ArrayList<>();
 
 		while (true) {
 			System.out.print("명령어 ) ");
@@ -19,12 +22,27 @@ public class App {
 			if (cmd.equals("종료")) {
 				break;
 			} else if (cmd.equals("등록")) {
+				int id = lastId + 1;
 				System.out.print("명언 : ");
 				String content = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String author = sc.nextLine().trim();
-				System.out.printf("%d번 명언이 등록되었습니다.", lastId);
-				lastId++;
+
+				WiseSaying wiseSaying = new WiseSaying(id, content, author);
+				wiseSayings.add(wiseSaying);
+
+				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+				lastId++;// 마지막 번호 증가
+
+			} else if (cmd.equals("목록")) {
+				if (wiseSayings.size() == 0) {
+					System.out.println("등록 된 명언이 없어");
+				} else {
+					System.out.println("있던데???");
+					System.out.println("등록 된 명언 수 : " + wiseSayings.size());
+				}
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
 	}
